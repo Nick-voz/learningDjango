637b5987900d50cdc9f49800d6d645989a81ebfd
«Nick-voz» <nik.vozisow@gmail.com>
2024-03-07 18:26:15 +0300

    refactor dock string

--- myShop/catalog/views.py
+++ myShop/catalog/views.py
@@ -3,5 +3,5 @@ from django.http.response import HttpResponse
 
 
 def catalog_home(request: HttpRequest):
-    "mock fiuc for path: './catalog/"
+    """mock func for path: './catalog/"""
     return HttpResponse('<h1> catalog home page </h1>')
