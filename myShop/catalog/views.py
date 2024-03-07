2af32ee858db7a996a5eff22ee28feda9bd24e2e
«Nick-voz» <nik.vozisow@gmail.com>
2024-03-07 16:33:12 +0300

    change format

--- myShop/catalog/views.py
+++ myShop/catalog/views.py
@@ -2,9 +2,6 @@ from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 
 
-# Create your views here.
-
-
 def catalog_home(request: HttpRequest):
     "mock fiuc for path: './catalog/"
     return HttpResponse('<h1> catalog home page </h1>')
